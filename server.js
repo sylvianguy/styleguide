2333e499caf8d0633c624abaf726eba8694a3797
Sylvia Nguyen <sylvia.nguyen12@gmail.com>
2017-07-07 17:56:17 -0400

    removed commented code from server.js

--- server.js
+++ server.js
@@ -22,16 +22,5 @@ app.use(webpackMiddleware(webpack(require('./webpack.config.js'))));
 // Serve your API assets here. You'll need to include the post route file.
 app.use(express.static('public'));
 
-// Serve your static assets here. You'll need to use express.static middleware.
-// app.use('/api/styleguide', require('./api'));
-// require('./api/models/styleguide');
-
-app.get('/api/styleguide', (req,res) => {
-	res.send('Notes Api');
-});
-
-// If none of the above matches, serve public/index.html.
-// app.get('*', (req, res) => res.sendFile(__dirname + '/public/index.html'))
-
 app.listen(3500);
 
