412334f57d392fb26c4ec1867f1967dfd1b786e9
Sylvia Nguyen <sylvia.nguyen12@gmail.com>
2017-07-08 13:31:03 -0400

    changed text

--- entry.js
+++ entry.js
@@ -4,15 +4,25 @@ import { ajax } from 'jquery';
 
 
 class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			data: []
+		}
+	}
 	componentDidMount() {
 		ajax({
 			url: "http://localhost:3500/api/styleguide/get",
 			method:'GET' 
 		})
-		.then(console.log)
+		.then((res) => {
+			this.setState({
+				data: res
+			})
+		});
 	}
 	render() {
-		return <h1>Hesads is this working?</h1>
+		return <h1>Stuff goes here eventually</h1>
 	}
 }
 
